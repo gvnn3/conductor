42f410bee5dc50aa4c1bb62138e43d10fbf7f234
George V. Neville-Neil <gnn@neville-neil.com>
2024-12-12 17:48:10 -0500

    Use the PID as the index.

--- Go/conduct/conduct.go
+++ Go/conduct/conduct.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/go-ini/ini"
 )
@@ -20,7 +19,7 @@ func main() {
 	if err != nil {
 		log.Println(err.Error())
 	}
-	clientConfigs.SetUniqueFlake(uint64(66)) // Use a random number for multiple tests
+	clientConfigs.SetUniqueFlake(uint64(os.Getpid())) // Use a random number for multiple tests
 	// Open a directory and get a handle
 	ok, fd := clientConfigs.Open("/ls/ChubbyCell_Conductor")
 	if ok {
@@ -38,14 +37,6 @@ func main() {
 		fmt.Printf("Create Error!\n")
 	}
 
-	// Delete the handle, note that the handle is only created by create and deleted by delete
-	ok = clientConfigs.Delete(fileFd, BaseServer.Opdelete)
-	if ok {
-		fmt.Printf("Delete file(%s) success\n", filename)
-	} else {
-		fmt.Printf("Delete Error!\n")
-	}
-
 	// Lock the newly created file
 	ok, token := clientConfigs.Acquire(fileFd, BaseServer.ReadLock, 0)
 	if ok {
@@ -54,7 +45,6 @@ func main() {
 		fmt.Printf("Acquire Error!\n")
 	}
 
-	// Delete the file with the token you locked yourself
 	ok = clientConfigs.Release(fileFd, token)
 	if ok {
 		fmt.Printf("release (%s) success.\n", filename)
@@ -62,61 +52,7 @@ func main() {
 		fmt.Printf("Release Error!\n")
 	}
 
-	ok = clientConfigs.Release(fileFd, token)
-	if ok {
-		fmt.Printf("release (%s) success.\n", filename)
-	} else {
-		fmt.Printf("Release Error!\n")
-	}
-
-	ok, token = clientConfigs.Acquire(fileFd, BaseServer.WriteLock, 1000)
-	if ok {
-		fmt.Printf("Acquire (%s) success, Token is %d\n", filename, token)
-	} else { // Obviously, you can't add a read lock after adding a write lock
-		fmt.Printf("WriteLock Error!\n")
-	}
-	// There will be problems when requesting data with this token after timeout, TODO but the request data with token has not been implemented yet
-
-	// Fail first
-	ok, token = clientConfigs.Acquire(fileFd, BaseServer.WriteLock, 0)
-	if ok {
-		fmt.Printf("Acquire (%s) success, Token is %d\n", filename, token)
-	} else { // Obviously, you can't add a read lock after adding a write lock
-		fmt.Printf("WriteLock Error!\n")
-	}
-
-	// After 2000ms, you can lock again successfully because the previous lock has timed out
-	time.Sleep(2000 * time.Millisecond)
-
-	ok, token = clientConfigs.Acquire(fileFd, BaseServer.WriteLock, 0)
-	if ok {
-		fmt.Printf("Acquire (%s) success, Token is %d\n", filename, token)
-	} else { // Obviously, you can't add a read lock after adding a write lock
-		fmt.Printf("WriteLock Error!\n")
-	}
-
-	ok = clientConfigs.Release(fileFd, token)
-	if ok {
-		fmt.Printf("release (%s) success.\n", filename)
-	} else {
-		fmt.Printf("Release Error!\n")
-	}
-
-	// Using the token that has been unlocked above should return false
-	ok = clientConfigs.CheckToken(fileFd.PathName, token)
-	if ok {
-		fmt.Printf("CheckToken error! filename(%s)\n", fileFd.PathName)
-	} else {
-		fmt.Printf("CheckToken success!\n")
-	}
-
-	// Finally, delete the file to facilitate testing lock_expand.go
-	ok = clientConfigs.Delete(fileFd, BaseServer.Opdelete)
-	if ok {
-		fmt.Printf("Delete file(%s) success\n", filename)
-	} else {
-		fmt.Printf("Delete Error!\n")
-	}
+	return
 
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: conduct <config_file>")
--- Go/play/player.go
+++ Go/play/player.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"ChubbyGo/BaseServer"
+	_ "ChubbyGo/Config"
+	"ChubbyGo/Connect"
+	"os"
+
 	"bytes"
 	"encoding/gob"
 	"fmt"
+	"log"
 	"net"
 )
 
@@ -51,6 +57,38 @@ func (p *Player) Run() {
 }
 
 func main() {
+	clientConfigs := Connect.CreateClient()
+	err := clientConfigs.StartClient()
+	if err != nil {
+		log.Println(err.Error())
+	}
+	clientConfigs.SetUniqueFlake(uint64(os.Getpid())) // Use a random number for multiple tests
+
+	filename := "/ls/ChubbyCell_Conductor/test.sh"
+	// Open a directory and get a handle
+	ok, fileFd := clientConfigs.Open(filename)
+	if ok {
+		fmt.Printf("Get fd success, instanceSeq is %d\n", fileFd.InstanceSeq)
+	} else {
+		fmt.Printf("Error!\n")
+	}
+
+	// Lock the newly created file
+	ok, token := clientConfigs.Acquire(fileFd, BaseServer.ReadLock, 0)
+	if ok {
+		fmt.Printf("Acquire (%s) success, Token is %d\n", filename, token)
+	} else {
+		fmt.Printf("Acquire Error!\n")
+	}
+
+	// Delete the file with the token you locked yourself
+	ok = clientConfigs.Release(fileFd, token)
+	if ok {
+		fmt.Printf("release (%s) success.\n", filename)
+	} else {
+		fmt.Printf("Release Error!\n")
+	}
+
 	player := NewPlayer("localhost", "8080")
 	player.Run()
 }
